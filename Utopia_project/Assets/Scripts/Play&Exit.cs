6fe1ddb74821864566b42baf38e420412403214e
Ormp <communists.1909@mail.ru>
2024-04-14 16:50:57 +0300

    Sixth_commit.
    Customized buttons; added sprites

--- Utopia_project/Assets/Scripts/Play&Exit.cs
+++ Utopia_project/Assets/Scripts/Play&Exit.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayExit : MonoBehaviour
 {
     public void PlayGame()
     {
