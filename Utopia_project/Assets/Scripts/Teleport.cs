d5e53ed9b1c0cdbe8f1cc4c75f2ecdcf3b4fef73
Ha6per <Ha6per@gmail.com>
2024-04-19 09:47:19 +0300

    25
    FirstScene

--- Utopia_project/Assets/Scripts/Teleport.cs
+++ Utopia_project/Assets/Scripts/Teleport.cs
@@ -7,7 +7,7 @@ public class TeleportToScene : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Shaman"))
         {
             SceneManager.LoadScene(sceneNumber);
         }
